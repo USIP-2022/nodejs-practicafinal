a32caaeb63e55d9f9a7f9f231c608a5ab5c6050d
yacel100 <master.yac@gmail.com>
2022-09-17 11:09:28 -0400

    :construction: realizando la configuracion de las excepciones

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 const express = require("express");
 const morgan = require("morgan");
+const productRouter = require("./routes/productRoutes");
+const MyError = require("./utils/MyError");
+
 const app = express();
 
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
+//manejo de errores
+app.all("*", (req, res, next) => {
+    next(new MyError("route not found", 404));
+});
+
+app.use((err, req, res, next) => {
+    if (process.env.NODE_ENV === "development") {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    } else {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: "Ocurrio un error",
+        });
+    }
+});
+
+//routes
+app.use("/api/v1/products/", productRouter);
+
 module.exports = app;
+
---
+++ controllers/productController.js
@@ -0,0 +1,40 @@
+const Product = require("../models/Product");
+
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    status: "success",
+    timeOfRequest: req.requestTime,
+    results: products.length,
+    data: {
+      products,
+    },
+  });
+};
+
+exports.addProduct = async (req, res) => {
+  const newProduct = await Product.create(req.body);
+  res.status(200).json({
+    status: "success",
+    data: {
+      product: newProduct,
+    },
+  });
+};
+
+exports.getProductById = async (req, res) => {
+  const foundProduct = await Product.findById(req.params.id);
+  if (foundProduct) {
+    res.status(200).json({
+      status: "success",
+      data: {
+        product: foundProduct,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: "not found",
+    });
+  }
+};
