272a554e8e73f8ff0cbe8a60041962c03b07bf54
yacel100 <master.yac@gmail.com>
2022-09-17 12:21:51 -0400

    :construction: Autenticacion del usuario creado (login)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const productRouter = require("./routes/productRoutes");
 const userRouter = require("./routes/userRoutes");
+const authRouter = require("./routes/authRoutes");
 const MyError = require("./utils/MyError");
 
 const app = express();
@@ -19,6 +20,9 @@ app.use((req, res, next) => {
 app.use("/api/v1/products/", productRouter);
 //Users
 app.use("/api/v1/users/", userRouter);
+//Auth
+app.use("/api/v1/auth/", authRouter);
+
 
 //manejo de errores
 app.all("*", (req, res, next) => {
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const productController = require("./../controllers/productController");
+const authController = require("./../controllers/authController");
+
 const productRouter = express.Router();
 //routes
 productRouter
   .route("/")
-  .get(productController.getAllProducts)
+    .all(authController.protect)
+    .get(productController.getAllProducts)
   .post(productController.addProduct);
 productRouter.route("/:id").get(productController.getProductById);
 
