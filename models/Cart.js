c7ff8561be68d27f51cc3bcbdb03bd2ce850b6bd
yacel100 <master.yac@gmail.com>
2022-09-19 00:07:58 -0400

    :construction: modificando variables para el carrito

--- models/Cart.js
+++ models/Cart.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
+const cartSchema = new mongoose.Schema({
   user: {
     type: String,
     required: true
@@ -15,5 +15,5 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const Cart = mongoose.model("Cart", userSchema);
+const Cart = mongoose.model("Cart", cartSchema);
 module.exports = Cart;
