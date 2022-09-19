85fa8e9b94d3962af24e1f67311ec435e0dfada3
yacel100 <master.yac@gmail.com>
2022-09-19 00:01:48 -0400

    :construction: agregando las rutas y funciones del controlador del carrito de compras v1

---
+++ controllers/cartController.js
@@ -0,0 +1,44 @@
+const User = require("../models/Cart");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllCart = catchAsync(async (req, res) => {
+    const carts = await Cart.find();
+
+    res.status(200).json({
+        status: "success",
+        timeOfRequest: req.requestTime,
+        results: carts.length,
+        data: {
+            carts,
+        },
+    });
+});
+
+exports.addCartProduct = catchAsync(async (req, res) => {
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            //TODO: falta realizar el metodo
+        },
+    });
+});
+exports.deleteCartProduct = catchAsync(async (req, res) => {
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            //TODO: falta realizar el metodo
+        },
+    });
+});
+
+exports.payCartProduct = catchAsync(async (req, res) => {
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            //TODO: falta realizar el metodo
+        },
+    });
+});
---
+++ models/Cart.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ["PENDING", "PAID"],
+    required: true
+  },
+  products: {
+    type: String,
+    required: true
+  },
+});
+
+const User = mongoose.model("Cart", userSchema);
+module.exports = User;
